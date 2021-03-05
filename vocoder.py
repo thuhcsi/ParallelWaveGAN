3459a711ec29c9658138ea3b921d827f66602e2c
lizzy <18811781486@163.com>
2021-03-05 18:23:15 +0800

    Load config & keep mel as numpy

--- vocoder.py
+++ vocoder.py
@@ -1,28 +1,48 @@
+
+import os
+
 import torch
 import yaml
+
 from parallel_wavegan.utils import load_model
 
 
 class Vocoder:
-    def __init__(self, checkpoint, config):
+    def __init__(self, checkpoint, config=None):
         """
-        
         Parameters
         ----------
-        checkpoint: str, the path of model checkpoint file.
-        config: str, the path of model configuration file.
-
+            checkpoint: str, the path of model checkpoint file.
+            config: str, the path of model configuration file.
         """
-        self.model = load_model(checkpoint).to("cuda").eval()
-        self.model.remove_weight_norm()
+
+        # load config
+        if config is None:
+            dirname = os.path.dirname(checkpoint)
+            config = os.path.join(dirname, "config.yml")
         with open(config) as f:
-            self.config = yaml.load(f, Loader=yaml.Loader)            
+            self._config = yaml.load(f, Loader=yaml.Loader)
+
+        # setup model
+        if torch.cuda.is_available():
+            self._device = torch.device("cuda")
+        else:
+            self._device = torch.device("cpu")
+        self._model = load_model(checkpoint, self._config)
+        self._model.remove_weight_norm()
+        self._model = self._model.eval().to(self._device)
+
 
     def mel2wav(self, mel):
         """
         Parameters
         ----------
-        mel: numpy.ndarray of mel spectrogram
+            mel: numpy.ndarray of mel spectrogram [shape=(#spec_frame, @n_mels)]
+
+        Returns
+        -------
+            wav: numpy.ndarray of generated waveform in [-1, 1] [shape=(#sample_point,)]
         """
         with torch.no_grad():
-            return self.model.inference(mel).view(-1).cpu().numpy()
+            mel = torch.tensor(mel, dtype=torch.float).to(self._device)
+            return self._model.inference(mel).view(-1).cpu().numpy()
